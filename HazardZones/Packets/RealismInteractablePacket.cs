81f799f699b64ffda1261e1f263e458064b767bd
Primal-13 <primal@levelcrush.com>
2025-03-08 18:57:41 -0500

    Initial versions of Interactable Packet and Rotate Suck Packet

---
+++ HazardZones/Packets/RealismInteractablePacket.cs
@@ -0,0 +1,30 @@
+﻿using LiteNetLib.Utils;
+
+namespace RealismModSync.HazardZones.Packets;
+
+public class RealismInteractablePacket : INetSerializable
+{
+    public RealismMod.EInteractableState InteractableState;
+    public RealismMod.EIneractableType InteractableType;
+    public RealismMod.EIneractableAction InteractableAction;
+    public string ZoneKey;
+    public string InteractionZoneName;
+    
+    public void Serialize(NetDataWriter writer)
+    {
+        writer.Put(ZoneKey);
+        writer.Put(InteractionZoneName);
+        writer.Put((int)InteractableType);
+        writer.Put((int)InteractableState);
+        writer.Put((int)InteractableAction);
+    }
+
+    public void Deserialize(NetDataReader reader)
+    {
+        ZoneKey = reader.GetString();
+        InteractionZoneName = reader.GetString();
+        InteractableType = (RealismMod.EIneractableType)reader.GetInt();
+        InteractableState = (RealismMod.EInteractableState)reader.GetInt();
+        InteractableAction = (RealismMod.EIneractableAction)reader.GetInt();
+    }
+}
---
+++ HazardZones/Packets/RealismRotateStuckPacket.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using LiteNetLib.Utils;
+
+namespace RealismModSync.HazardZones.Packets;
+
+public class RealismRotateStuckPacket : INetSerializable
+{
+    public string ZoneKey;
+    public string InteractionZoneName;
+    public float Direction;
+
+    public void Serialize(NetDataWriter writer)
+    {
+        writer.Put(ZoneKey);
+        writer.Put(InteractionZoneName);
+        writer.Put(Direction);
+    }
+
+    public void Deserialize(NetDataReader reader)
+    {
+        ZoneKey = reader.GetString();
+        InteractionZoneName = reader.GetString();
+        Direction = reader.GetFloat();
+    }
+    
+}
