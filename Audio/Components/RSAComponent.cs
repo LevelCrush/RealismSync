e2d14ae215aac263f267adfa76c4307ce2495e87
Primal-13 <primal@levelcrush.com>
2025-03-08 23:15:00 -0500

    RSAComponent start

---
+++ Audio/Components/RSAComponent.cs
@@ -0,0 +1,40 @@
+﻿using Fika.Core.Coop.Players;
+using Fika.Core.Coop.Utils;
+using Fika.Core.Networking;
+using RealismMod;
+using UnityEngine;
+
+namespace RealismModSync.Audio.Components;
+
+public class RSAComponent : MonoBehaviour
+{
+    CoopPlayer player;
+    
+    private AudioSource _foodPoisoningSfx;
+    private AudioSource _gasMaskAudioSource;
+    private AudioSource _gasAnalyserSource;
+    private AudioSource _geigerAudioSource;
+    private AudioSource _toggleDeviceSource;
+    
+    private void Start()
+    {
+        player = GetComponent<CoopPlayer>();
+        if(player == null)
+        {
+            Destroy(this);
+            return;
+        }
+    }
+    
+    // copy pasted from RealismMod and adjusted for RealismSync if needed
+    private void SetUpAudio(AudioSource source, float vol = 1f, float spatialBlend = 1f, float minDistance = 1f, float maxDistance = 30f) 
+    {
+        source.volume = vol * GameWorldController.GetGameVolumeAsFactor();
+        source.spatialBlend = spatialBlend; 
+        source.minDistance = minDistance;
+        source.maxDistance = maxDistance;
+        source.spatialize = true;
+        source.rolloffMode = AudioRolloffMode.Linear; // for now try linear?
+    }
+
+}
